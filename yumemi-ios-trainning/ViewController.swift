0a75e92394bbb9398e6c8589dd254bd8e76433a2
kitamura5897 <101320377+kitamura5897@users.noreply.github.com>
2023-02-19 11:06:58 +0900

    first commit

---
+++ yumemi-ios-trainning/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  yumemi-ios-trainning
+//
+//  Created by 喜多村海人 on 2023/02/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
